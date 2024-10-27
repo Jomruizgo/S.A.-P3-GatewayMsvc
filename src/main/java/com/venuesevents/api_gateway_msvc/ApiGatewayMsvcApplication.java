fd1c234d7a5903c63e73793b00d3d5ddd5bb4ce1
Jorge <jomruizgo@gmail.com>
2024-10-27 02:25:40 -0500

    first commit

---
+++ src/main/java/com/venuesevents/api_gateway_msvc/ApiGatewayMsvcApplication.java
@@ -0,0 +1,13 @@
+package com.venuesevents.api_gateway_msvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMsvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venuesevents/api_gateway_msvc/ApiGatewayMsvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venuesevents.api_gateway_msvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayMsvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
